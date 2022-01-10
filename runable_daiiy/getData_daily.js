697da40a3efb987e7bda2ac6de8e85b7883dd931
hm <>
2022-01-10 13:11:21 +0800

    修改

--- runable_daiiy/getData_daily.js
+++ runable_daiiy/getData_daily.js
@@ -15,12 +15,12 @@ workbook.creator = 'One of the best';
 workbook.lastModifiedBy = 'One of the best';
 workbook.created = new Date();
 
-let file = `/Users/hm/Desktop/football相关/球探数据${monent(new Date()).format('YYYY-MM-DD')}.xls`
 
 let catchURL = async (msg) => {
     try {
         let game = JSON.parse(msg.body)
         console.log('game===> ', game)
+        let file = `/Users/hm/Desktop/football相关/球探数据${monent(new Date()).format('YYYY-MM-DD')}.xls`
         let xurl = game.xurl
         const proxy_types = ['http']
         const proxyType = proxy_types[Math.floor(Math.random() * proxy_types.length)]
@@ -62,26 +62,6 @@ let catchURL = async (msg) => {
 
         let note = $('#table_v > tbody > tr:last-child').text().replace(/\t*/g, '').trim() //拿到析页面战绩数据
 
-        /*let json_x = {}
-        let arr_x = []
-        $('#table_v > tbody tr').map((a, b) => {
-            if ($(b).attr('id')) {
-                arr_x = []
-                $(b).find('td').map((e, f) => {
-                    arr_x.push($(f).text())
-                })
-                json_x[a] = arr_x
-            }
-        })
-        json_x['url'] = [game.xurl]
-        // 西甲-巴列卡诺(主)-平/半
-        // let team_name_x = $('div.analyhead > div.home > a').text()
-        let label_x = `${game.country}-${game.team}-${game.crown}析`.trim().replace(/\s/g, '')
-        // if (label_x.length > 30) {
-        //     label_x = label_x.substring(0, 29) + 'X'
-        // }
-        await wap_xls_x(json_x, label_x).then().catch(console.dir)
-        console.log('析====>写入成功')*/
 
         //欧欧欧欧欧欧
         let ourl = game.ourl
@@ -153,16 +133,11 @@ let catchURL = async (msg) => {
             })
             // console.log('json_o=====> ',json_o)
             if (Object.keys(json_o).length) {
-                json_o_avg = cal_avg(game, json_o, note)
+                json_o_avg = await utils.cal_avg(game, json_o, note)
             }
         }
         let label_o = `${game.country}-${game.team}-${game.crown}`.trim().replace(/\s/g, '')
-        // if (label_o.length > 30) {
-        //     label_o = label_o.substring(0, 29) + 'O'
-        // }
-        // json_o_avg['note'] = [`${game.score1} ${game.crown} ${game.score2}`, note]
-        // json_o_avg['url'] = [`${game.score1} ${game.crown} ${game.score2}`, game.ourl]
-        await wap_xls_o(json_o_avg, label_o).then().catch(console.dir)
+        await utils.wap_xls_o(workbook, file, json_o_avg, label_o).then().catch(console.dir)
         console.log('欧====>写入成功')
 
         await page.close()
@@ -171,168 +146,6 @@ let catchURL = async (msg) => {
         console.log('err===> ', e, JSON.parse(msg.body))
         utils.requeue(msg, config.delay)
     }
-}
-
-
-function cal_avg(game, json_o, note) {
-    let company_num = Object.values(json_o).length
-    let avg_arr = []
-    let temp3 = [], temp4 = [], temp5 = [], temp6 = [], temp7 = [], temp8 = [], temp9 = [],
-       temp10 = [], temp11 = [], temp12 = [], temp16 = [], temp17 = [], temp18 = [], temp20 = [],
-       temp21 = [], temp22 = [], temp24 = [], temp25 = [], temp26 = [], temp28 = [], temp29 = [],
-       temp30 = [], temp31 = []
-    Object.values(json_o).map((data, i) => {
-        for (let j = 0; j < data.length; j++) {
-            if (j === 3) temp3.push(data[j])
-            if (j === 4) temp4.push(data[j])
-            if (j === 5) temp5.push(data[j])
-            if (j === 6) temp6.push(data[j])
-            if (j === 7) temp7.push(data[j])
-            if (j === 8) temp8.push(data[j])
-            if (j === 9) temp9.push(data[j])
-            if (j === 10) temp10.push(data[j])
-            if (j === 11) temp11.push(data[j])
-            if (j === 12) temp12.push(data[j])
-            if (j === 16) temp16.push(data[j])
-            if (j === 17) temp17.push(data[j])
-            if (j === 18) temp18.push(data[j])
-            if (j === 20) temp20.push(data[j])
-            if (j === 21) temp21.push(data[j])
-            if (j === 22) temp22.push(data[j])
-            if (j === 24) temp24.push(data[j])
-            if (j === 25) temp25.push(data[j])
-            if (j === 26) temp26.push(data[j])
-            if (j === 28) temp28.push(data[j])
-            if (j === 29) temp29.push(data[j])
-            if (j === 30) temp30.push(data[j])
-            if (j === 31) temp31.push(data[j])
-        }
-    })
-    avg_arr.push(`${game.score1} ${game.crown} ${game.score2}`)
-    avg_arr.push(note)
-    avg_arr.push(`${game.ourl}`)
-    avg_arr.push(avg(temp3, company_num))
-    avg_arr.push(avg(temp4, company_num))
-    avg_arr.push(avg(temp5, company_num))
-    avg_arr.push(avg(temp6, company_num))
-    avg_arr.push(avg(temp7, company_num))
-    avg_arr.push(avg(temp8, company_num))
-    avg_arr.push(avg(temp9, company_num))
-    avg_arr.push(avg(temp10, company_num))
-    avg_arr.push(avg(temp11, company_num))
-    avg_arr.push(avg(temp12, company_num))
-    avg_arr.push('')
-    avg_arr.push('')
-    avg_arr.push('')
-    avg_arr.push(avg(temp16, company_num))
-    avg_arr.push(avg(temp17, company_num))
-    avg_arr.push(avg(temp18, company_num))
-    avg_arr.push('')
-    avg_arr.push(avg(temp20, company_num))
-    avg_arr.push(avg(temp21, company_num))
-    avg_arr.push(avg(temp22, company_num))
-    avg_arr.push('')
-    avg_arr.push(avg(temp24, company_num))
-    avg_arr.push(avg(temp25, company_num))
-    avg_arr.push(avg(temp26, company_num))
-    avg_arr.push('')
-    avg_arr.push(avg(temp28, company_num))
-    avg_arr.push(avg(temp29, company_num))
-    avg_arr.push(avg(temp30, company_num))
-    json_o['avg'] = avg_arr
-    return json_o
-}
-
-
-async function wap_xls_o(json_o, label_o) {
-    const worksheet = await workbook.addWorksheet(label_o, {properties: {tabColor: {argb: 'c30101'}}});
-    let cols = [
-        {header: '指数', key: 'index', width: 20},
-        {header: '胜率', key: 'odds', width: 75},
-        {header: '所有公司', key: 'company', width: 32},
-        {header: '主胜', key: 'home_win', width: 10},
-        {header: '和', key: 'draw', width: 10},
-        {header: '客胜', key: 'guest_win', width: 10},
-        {header: '主胜率', key: 'home_win_rate', width: 10},
-        {header: '和率', key: 'draw_rate', width: 10},
-        {header: '客胜率', key: 'guest_win_rate', width: 10},
-        {header: '返还率', key: 'return_rate', width: 10},
-        {header: '凯利指数', key: 'kelly_index1', width: 10},
-        {header: '凯利指数', key: 'kelly_index2', width: 10},
-        {header: '凯利指数', key: 'kelly_index3', width: 10},
-        {header: '变化时间', key: 'change_time', width: 12},
-        {header: '历史指数', key: 'history_index', width: 12},
-        {header: '', key: '', width: 12},  //间隔
-        {header: '主胜-1', key: '主胜-1', width: 12},
-        {header: '和-1', key: '和-1', width: 12},
-        {header: '客胜-1', key: '客胜-1', width: 12},
-        {header: '', key: '', width: 12},  //间隔
-        {header: '主胜-2', key: '主胜-2', width: 12},
-        {header: '和-2', key: '和-2', width: 12},
-        {header: '客胜-2', key: '客胜-2', width: 12},
-        {header: '', key: '', width: 12},  //间隔
-        {header: '主胜-3', key: '主胜-3', width: 12},
-        {header: '和-3', key: '和-3', width: 12},
-        {header: '客胜-3', key: '客胜-3', width: 12},
-        {header: '', key: '', width: 12},  //间隔
-        {header: '主胜-4', key: '主胜-4', width: 12},
-        {header: '和-4', key: '和-4', width: 12},
-        {header: '客胜-4', key: '客胜-4', width: 12} //29
-    ];
-    worksheet.columns = cols
-    Object.values(json_o).map(async (data) => {
-        await worksheet.addRow(wrap_ifo(cols, data))
-    })
-    await workbook.xlsx.writeFile(file)
-}
-
-
-async function wap_xls_x(json_x, team) {
-    const worksheet = await workbook.addWorksheet(team, {properties: {tabColor: {argb: 'c30101'}}});
-    let cols = [
-        {header: '类型', key: 'gameType', width: 10},
-        {header: '日期', key: 'date', width: 10},
-        {header: '主场', key: 'home', width: 15},
-        {header: '比分(半场)', key: 'score', width: 15},
-        {header: '角球', key: 'corner', width: 8},
-        {header: '客场', key: 'guest', width: 15},
-        {header: '主', key: 'home_1', width: 8},
-        {header: '盘口', key: 'mouth', width: 10},
-        {header: '客', key: 'guest_1', width: 8},
-        {header: '主', key: 'home_2', width: 8},
-        {header: '和', key: 'draw', width: 8},
-        {header: '客', key: 'guest_2', width: 8},
-        {header: '胜负', key: 'outcome', width: 8},
-        {header: '让球', key: 'letBall', width: 8},
-        {header: '进球数', key: 'into', width: 8},
-    ];
-
-    worksheet.columns = cols
-    Object.values(json_x).map(async (data) => {
-        await worksheet.addRow(wrap_ifo(cols, data))
-    })
-    await workbook.xlsx.writeFile(file)
-}
-
-function wrap_ifo(cols, data) {
-    let res = {}
-    cols.map((col, j) => {
-        // if (col.header == '主' || col.header == '客' || col.header == '和'
-        //    || col.header == '主胜' || col.header == '客胜' || col.header == '主胜率'
-        //    || col.header == '和率' || col.header === '客胜率' || col.header == '返还率'
-        //    || col.header == '凯利指数'
-        // ) {
-        //     res[col.key] = Number(data[j])
-        // } else {
-        res[col.key] = data[j]
-        // }
-    })
-    return res
-}
-
-function avg(arr, num) {
-    let sum = eval(arr.join("+"))
-    return (sum / num).toFixed(2)
 }
 
 
